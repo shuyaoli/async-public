fc98e65256142c30d81c461702e7359955e7c36d
Shuyao <shuyao@hku.hk>
2019-06-25 15:50:25 -0700

    Before changing z_v to atomic

---
+++ main_async_lock_free.m
@@ -0,0 +1,14 @@
+Initialize;
+
+tic
+
+db_trained = Finito_async_lock_free(x, y, alpha, s, epoch, 8);
+
+toc
+
+
+result = x * db_trained' > 0;
+result = 2 * result - 1;
+
+error_rate = 1 - sum(result == y) / size(result,1);
+fprintf('The cost is %.15f\n',f(db_trained', x, y, s));
