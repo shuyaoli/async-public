9537b80b536fabc851f9e6c5800f8fccefa52e02
Shuyao <shuyao@hku.hk>
2019-07-12 12:10:47 -0700

    Parallel-sum to floating point precision (7)

--- gd_cuda/main.cu
+++ gd_cuda/main.cu
@@ -41,7 +41,8 @@ void err_chk(cudaError err) {
 
 void read_var(double* var, string var_name, int len)
 {
-  string filename = string(SIZE) + string("/") + var_name + string(".txt");
+  string filename =
+    string(SIZE) + string("/") + var_name + string(".txt");
   ifstream var_file(filename);
   string line;
   if (!var_file.is_open()) {
@@ -59,7 +60,8 @@ void read_var(double* var, string var_name, int len)
   var_file.close();
 }
 
-__global__ void parallel_mean(const double* __restrict__ z, double *sum_z) {
+__global__ void parallel_sum(const double* __restrict__ z,
+                             double *sum_z) {
   //Holds intermediates in shared memory reduction
   __syncthreads();
   __shared__ double buffer[1024/WARP_SIZE];
@@ -98,7 +100,10 @@ __global__ void parallel_mean(const double* __restrict__ z, double *sum_z) {
   }
 }
 
-__global__ void zUpdate(const double* __restrict__ x_a, const double* __restrict__ y, double* z_a, const double* __restrict__ mean_z)
+__global__ void zUpdate(const double* __restrict__ x_a,
+                        const double* __restrict__ y,
+                        double* z_a,
+                        const double* __restrict__ mean_z)
 {
   const int ik = blockDim.x*blockIdx.x + threadIdx.x;
   
@@ -108,7 +113,8 @@ __global__ void zUpdate(const double* __restrict__ x_a, const double* __restrict
     dot += mean_z[i] * x_a[dim * ik + i];
 
   for (int c =  0; c < dim; c++) {        
-    z_a[ik+c*n] = mean_z[c] - alpha * (-1.0 / (1+exp(y[ik] * dot)) * y[ik] * x_a[dim * ik + c] + s * mean_z[c]);
+    z_a[ik+c*n] = mean_z[c] -
+      alpha * (-1.0 / (1+exp(y[ik] * dot)) * y[ik] * x_a[dim * ik + c] + s * mean_z[c]);
   }
   
 }
@@ -123,22 +129,20 @@ int main()
   
   double* z_a =  new double[n * dim]();
 
-  double* mean_z = new double [dim] ();
+  double* mean_z = new double [dim];
 
   // double *d_x_a, *d_y;
   double *d_z_a, *d_mean_z;
 
   // err_chk(cudaMalloc((void**)&d_x_a, sizeof(double) * n * dim));
   // err_chk(cudaMalloc((void**)&d_y, sizeof(double) * n ));
-  err_chk(cudaMalloc((void**)&d_z_a, sizeof(double) * n * dim));
-  err_chk(cudaMalloc((void**)&d_mean_z, sizeof(double) * dim));
-  cout << "CUDA Malloc done\n";
-  
+  err_chk(cudaMalloc(&d_z_a, sizeof(double) * n * dim));
+  err_chk(cudaMalloc(&d_mean_z, sizeof(double) * dim));
 
   // err_chk(cudaMemcpy(d_x_a, x_a, sizeof(double) * n * dim, cudaMemcpyHostToDevice));
   // err_chk(cudaMemcpy(d_y, y, sizeof(double) * n, cudaMemcpyHostToDevice));
 
-  cout << "CUDA Memcpy Host to Device Done\n";
+  // cout << "CUDA Memcpy Host to Device Done\n";
   
   for (int k = 0; k < epoch; k++) {
     
@@ -149,26 +153,37 @@ int main()
           dot += mean_z[i] * x_a[dim * ik + i];
         
         for (int c =  0; c < dim; c++) {        
-          z_a[ik+c*n] = mean_z[c] - alpha * (-1.0 / (1+exp(y[ik] * dot)) * y[ik] * x_a[dim * ik + c] + s * mean_z[c]);
-          if ( ik == 0 && k == epoch - 1) printf("%.15f\n", z_a[c*n]);
+          z_a[ik+c*n] = mean_z[c] - alpha *
+            (-1.0 / (1+exp(y[ik] * dot)) * y[ik] * x_a[dim * ik + c] + s * mean_z[c]);
         }
     }
-    
-    err_chk(cudaMemcpy(d_z_a, z_a, sizeof(double) * n * dim, cudaMemcpyHostToDevice));    
+
+    err_chk(cudaMemcpy(d_z_a, z_a, sizeof(double) * n * dim, cudaMemcpyHostToDevice));
+    memset(mean_z, 0, sizeof(float) * dim);
     err_chk(cudaMemcpy(d_mean_z, mean_z, sizeof(double) * dim, cudaMemcpyHostToDevice));
 
-    parallel_mean <<< n / 1024, 1024>>> (d_z_a, d_mean_z);
+    parallel_sum <<< n / 1024, 1024>>> (d_z_a, d_mean_z);
 
     err_chk(cudaMemcpy(mean_z, d_mean_z, sizeof(double) * dim, cudaMemcpyDeviceToHost));
+    
     for (int c = 0; c < dim; c++)
       mean_z[c] /= n;
-    err_chk(cudaMemcpy(d_mean_z, mean_z, sizeof(double) * dim, cudaMemcpyHostToDevice));
+
+
+    // for (int c = 0; c < dim; c++) {
+    //   double total = 0;
+    //   for (int r = 0; r < n; r++) {
+    //     total += z_a[r + c * n];
+    //   }
+    //   mean_z[c] = total / n;
+    // }
+
   }
 
   cudaFree(d_z_a);
   cudaFree(d_mean_z);
   
-  // for (int i = 0; i < dim; i++) printf("%.15f\n", mean_z[i]);
+  for (int i = 0; i < dim; i++) printf("%.15f\n", mean_z[i]);
 
   return 0;
 }
---
+++ parallel_sum/parallel_sum.cu
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <cassert>
+#include <ctime>
+#include <stdio.h>
+#include <cuda.h>
+
+
+__device__ double atomic_add(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                              (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
+                               __longlong_as_double(assumed)));
+
+    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+
+using namespace std;
+
+#define n 131072 
+#define d 512
+#define WARP_SIZE 32
+#define num_T float
+#define IS_DOUBLE 0
+
+/*********This is a workaround to the bug of Nvidia CUDA identified at this page***********/
+/*https://stackoverflow.com/questions/37566987/cuda-atomicadd-for-doubles-definition-error*/
+
+// typedef float num_T;
+
+__global__ void parallel_sum(const num_T* __restrict__ z, num_T *x_half) {
+  //Holds intermediates in shared memory reduction
+  __syncthreads();
+  __shared__ num_T buffer[1024/WARP_SIZE];
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int lane = threadIdx.x % WARP_SIZE;
+
+  for (int k=0; k<d; k++) {
+    int j = (k + blockIdx.x) % d;
+    //j = k;
+    num_T temp;
+    // All threads in a block of 1024 take an element
+    temp = z[i + n*j];
+    
+    // All warps in this block (32) compute the sum of all
+    // threads in their warp
+    for(int delta = WARP_SIZE/2; delta > 0; delta /= 2)
+      temp += __shfl_xor_sync(0xffffffff, temp, delta);
+
+
+    // Write all 32 of these partial sums to shared memory
+    if(lane == 0)
+      buffer[threadIdx.x / WARP_SIZE] = temp;
+    
+    __syncthreads();
+
+
+    // Add the remaining 32 partial sums using a single warp
+    if(threadIdx.x < WARP_SIZE) {
+      temp = buffer[threadIdx.x];
+      for(int delta = WARP_SIZE / 2; delta > 0; delta /= 2)
+        temp += __shfl_xor_sync(0xffffffff,temp, delta);
+    }
+
+    // Add this block's sum to the total sum
+    if(threadIdx.x == 0)
+# if IS_DOUBLE
+        atomic_add(x_half+j, temp);
+# else
+        atomicAdd(x_half+j, temp);
+#endif
+      // x_half[j] = temp;
+  }
+}
+
+void err_chk(cudaError err) {
+  if (err != cudaSuccess) {
+    cout << cudaGetErrorString(err) << endl;
+    assert(false);
+  }
+}
+
+int main() {
+
+  
+  num_T* x_half = new num_T[d]();
+  num_T* z = new num_T[d*n];
+
+  // memset(x_half,0,d*sizeof(num_T));
+  for (int i = 0; i < n * d; i ++)
+    z[i] = i;
+
+  num_T* d_x_half;
+  num_T* d_z;
+
+  err_chk(cudaMalloc(&d_x_half, sizeof(num_T)*d));
+  err_chk(cudaMalloc(&d_z, sizeof(num_T)*n*d));
+  err_chk(cudaMemcpy(d_z, z, sizeof(num_T)*n*d, cudaMemcpyHostToDevice));
+    
+
+  // err_chk(cudaMemcpy(d_x_half, x_half, sizeof(num_T)*d, cudaMemcpyHostToDevice));
+                                        
+  parallel_sum <<<n/1024,1024>>> (d_z, d_x_half);
+
+  err_chk(cudaMemcpy(x_half, d_x_half, sizeof(num_T)*d, cudaMemcpyDeviceToHost));
+  cudaFree(d_z);
+  cudaFree(d_x_half);
+  for (int i = 0; i < 10; i++)
+    printf("%.1f\n", x_half[i]);
+  return 0;
+}
+
+
+    
